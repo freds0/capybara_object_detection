6391aecb914a2af09b933e6c51e865dc3793a3cf
Fred <fred_s0@yahoo.com.br>
2022-04-22 12:06:33 -0300

    Corrections
    Adding automatic num_classes and classes names from yaml file.

--- utils/class_to_int.py
+++ utils/class_to_int.py
@@ -1,7 +1,10 @@
 
-def class_label_to_int(row_label):
-    if row_label == 'capivara':
-        return 1
-    else:
+def class_label_to_int(row_label, labels_list):
+
+    if row_label not in labels_list:
         print("Error: " + row_label)
-        None
+        return None
+
+    for index, class_label in enumerate(labels_list):
+        if row_label == class_label:
+            return index + 1 # Index must start with 1
